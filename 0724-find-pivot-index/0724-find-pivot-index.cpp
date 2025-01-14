08eb866dc1d29e254dd2e86542144ab81f4a5843
Ritik-Kumar01 <118629689+Ritik-Kumar01@users.noreply.github.com>
2025-01-14 17:51:00 +0530

    Time: 4 ms (19.32%), Space: 35.5 MB (80.87%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,51 +1,51 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        // int rightsum = 0;
+        int rightsum = 0;
 
-        // for(int num : nums){
-        //     rightsum += num;
-        // }
+        for(int num : nums){
+            rightsum += num;
+        }
 
-        // int leftsum = 0;
+        int leftsum = 0;
 
-        // for(int i = 0 ; i<nums.size();i++){
+        for(int i = 0 ; i<nums.size();i++){
          
-        //  rightsum -= nums[i];
+         rightsum -= nums[i];
 
-        //  if(leftsum == rightsum){
-        //     return i;
-        //  }
-
-        //  leftsum += nums[i];
-
-        // }
-        // return -1;
-
-
-        int n = nums.size();
-    
-    // Iterate through each index
-    for (int i = 0; i < n; ++i) {
-        // Calculate left sum
-        int leftSum = 0;
-        for (int j = 0; j < i; ++j) {
-            leftSum += nums[j];
-        }
-        
-        // Calculate right sum
-        int rightSum = 0;
-        for (int j = i + 1; j < n; ++j) {
-            rightSum += nums[j];
-        }
-        
-        // Check if left sum equals right sum
-        if (leftSum == rightSum) {
+         if(leftsum == rightsum){
             return i;
+         }
+
+         leftsum += nums[i];
+
         }
-    }
+        return -1;
+
+
+    //     int n = nums.size();
     
-    // Return -1 if no pivot index found
-    return -1;
+    // // Iterate through each index
+    // for (int i = 0; i < n; ++i) {
+    //     // Calculate left sum
+    //     int leftSum = 0;
+    //     for (int j = 0; j < i; ++j) {
+    //         leftSum += nums[j];
+    //     }
+        
+    //     // Calculate right sum
+    //     int rightSum = 0;
+    //     for (int j = i + 1; j < n; ++j) {
+    //         rightSum += nums[j];
+    //     }
+        
+    //     // Check if left sum equals right sum
+    //     if (leftSum == rightSum) {
+    //         return i;
+    //     }
+    // }
+    
+    // // Return -1 if no pivot index found
+    // return -1;
     }
 };
