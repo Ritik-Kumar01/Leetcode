aeb79aed65b5a2458b8c5961b1cc6e1dd517c743
Ritik-Kumar01 <118629689+Ritik-Kumar01@users.noreply.github.com>
2025-01-14 16:28:39 +0530

    Time: 0 ms (100%), Space: 24.9 MB (5.15%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,15 +3,37 @@ public:
     void moveZeroes(vector<int>& nums) {
         
 
-      int insertposition = 0;
-      for(int i=0; i<nums.size();i++){
-        if(nums[i]!=0){
-            nums[insertposition++] = nums[i];
-        }
-      }  
+    //   int insertposition = 0;
+    //   for(int i=0; i<nums.size();i++){
+    //     if(nums[i]!=0){
+    //         nums[insertposition++] = nums[i];
+    //     }
+    //   }  
 
-      while(insertposition<nums.size()){
-        nums[insertposition++]=0;
+    //   while(insertposition<nums.size()){
+    //     nums[insertposition++]=0;
+    //   }
+
+
+     vector<int> nonzero;
+     vector<int> zero;
+
+     for(int num : nums){
+      if(num != 0){
+        nonzero.push_back(num);
       }
+
+      else{
+        zero.push_back(num);
+      }
+     }
+
+    int index = 0;
+    for(int num : nonzero){
+        nums[index++] = num;
+    }
+    for(int num : zero){
+        nums[index++] = num;
+    }
     }
 };
