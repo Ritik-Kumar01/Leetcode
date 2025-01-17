c28124969267fa12148ee29d15ca626245e52f05
Ritik-Kumar01 <118629689+Ritik-Kumar01@users.noreply.github.com>
2025-01-17 14:36:27 +0530

    Time: 11 ms (88.05%), Space: 43.3 MB (5.01%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
     string removeStars(string s) {
-        stack<char> st;
+        // using stack
 
-        for(int i = 0 ; i<s.length();i++){
-            if(s[i]=='*'){
-                st.pop();
-            }
-            else{
-                st.push(s[i]);
-            }
-        }
+        // stack<char> st;
 
-        string result;
-
-        while(!st.empty()){
-            result.push_back(st.top());
-            st.pop();
-        }
-
-        reverse(begin(result),end(result));
-        return result;
+        // for(int i = 0 ; i<s.length();i++){
+        //     if(s[i]=='*'){
+        //         st.pop();
+        //     }
+        //     else{
+        //         st.push(s[i]);
+        //     }
+        // }
 
         // string result;
 
+        // while(!st.empty()){
+        //     result.push_back(st.top());
+        //     st.pop();
+        // }
+
+        // reverse(begin(result),end(result));
+        // return result;
+
+        // Using string
+
+        // string result;
         // for(char ch : s){
         //     if(ch=='*'){
         //         if(!result.empty()){
@@ -35,5 +38,30 @@ public:
         //     }
         // }
         // return result;
+
+        //using pointer 
+
+        int n = s.length();
+
+        vector<int> temp(n);
+
+        int j = 0;
+
+        for(int i = 0; i<n;i++){
+            if(s[i]=='*'){
+                j--;
+            }
+            else{
+                temp[j] = s[i];
+                j++;
+            }
+        }
+
+        string result = "";
+
+        for(int k = 0 ;k<=j-1;k++){
+            result.push_back(temp[k]);
+        }
+        return result;
     }
 };
