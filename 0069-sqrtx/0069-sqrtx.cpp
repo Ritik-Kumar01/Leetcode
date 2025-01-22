2710c9deeab4ab5b034f252fed8c89448f3b7006
Ritik-Kumar01 <118629689+Ritik-Kumar01@users.noreply.github.com>
2025-01-22 11:13:27 +0530

    Time: 0 ms (100%), Space: 8.7 MB (13.7%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+       if (x == 0 || x == 1) return x; 
+
+    int left = 0, right = x, result = 0;
+
+    while (left <= right) {
+        int mid = left + (right-left) / 2;
+
+       
+        if (1LL * mid * mid <= x) {
+            result = mid;
+            left = mid + 1; 
+        } else {
+            right = mid - 1; 
+        }
+    }
+
+    return result;
+        
+    }
+};
