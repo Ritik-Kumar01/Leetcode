dd0247c0d8e3890cc163804bf9b4bd4138622c4e
Ritik-Kumar01 <118629689+Ritik-Kumar01@users.noreply.github.com>
2025-01-27 17:21:59 +0530

    Time: 29 ms (94.96%), Space: 104.5 MB (17.82%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
@@ -2,19 +2,46 @@ class Solution {
 public:
     vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
        
-       int count = 0;
-       vector<int> vec;
-        for(int i=0;i<spells.size();i++){
-            count = 0;
-            for(int j = 0 ; j<potions.size();j++){
-                if((long long)spells[i]*potions[j]>=success){
-                    count++;
+    //    int count = 0;
+    //    vector<int> vec;
+    //     for(int i=0;i<spells.size();i++){
+    //         count = 0;
+    //         for(int j = 0 ; j<potions.size();j++){
+    //             if((long long)spells[i]*potions[j]>=success){
+    //                 count++;
                     
-                }
-            }
-                vec.push_back(count);
-        }
+    //             }
+    //         }
+    //             vec.push_back(count);
+    //     }
      
-        return vec;
+    //     return vec;
+
+
+    int m = spells.size();
+    int n = potions.size();
+    
+    sort(begin(potions),end(potions));
+
+    int maxPotionvalue = potions[n-1];
+    vector<int> ans;
+
+    for(int i =0 ; i<m ;i++){
+        int spell = spells[i];
+         
+         // minPotion * spell >=success
+        long long minPotion = ceil((1.0*success)/spell);
+
+        if(minPotion> maxPotionvalue){
+            ans.push_back(0);
+            continue;
+        }
+
+        int index = lower_bound(begin(potions),end(potions),minPotion) - begin(potions);
+
+        int count = n -index; 
+        ans.push_back(count);
+    }
+    return ans;
     }
 };
