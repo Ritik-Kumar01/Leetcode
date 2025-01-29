a101841eed5e6e2c8417dc5d6372be7b720a34ec
Ritik-Kumar01 <118629689+Ritik-Kumar01@users.noreply.github.com>
2025-01-29 17:56:56 +0530

    Time: 0 ms (100%), Space: 14.9 MB (63.23%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -9,27 +9,67 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//   // DFS approach
+//    void preorder(TreeNode* &root,int level,vector<int>&result){
+//     if(root==NULL) return;
+
+//     if(result.size()<level){
+//         result.push_back(root->val);
+//     }
+
+//     preorder(root->right,level+1,result);
+//     preorder(root->left,level+1,result);
+//    }
+
+//     vector<int> rightSideView(TreeNode* root) {
+
+
+//         vector<int> result;
+
+//         preorder(root,1,result);
+//         return result;
+        
+//     }
+// };
+
+
+
+
+
 class Solution {
 public:
-
-   void preorder(TreeNode* &root,int level,vector<int>&result){
-    if(root==NULL) return;
-
-    if(result.size()<level){
-        result.push_back(root->val);
-    }
-
-    preorder(root->right,level+1,result);
-    preorder(root->left,level+1,result);
-   }
+  
 
     vector<int> rightSideView(TreeNode* root) {
+     
+     if(root==NULL) return{};
+    
+    vector<int> result;
+     queue<TreeNode*> que;
+     que.push(root);
+
+     while(!que.empty()){
+        int n = que.size();
+        TreeNode* node =NULL;
+
+        while(n--){
+            node = que.front();
+            que.pop();
+
+        if(node->left!=NULL){
+            que.push(node->left);
+        }
+        if(node->right!=NULL){
+            que.push(node->right);
+        }
+        }
 
 
-        vector<int> result;
-
-        preorder(root,1,result);
-        return result;
-        
+     result.push_back(node->val);
+     }
+     return result;
+   
     }
 };
