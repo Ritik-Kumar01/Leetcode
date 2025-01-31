08c7c27aa2cabbf3fd95fae74e6001378fe99080
Ritik-Kumar01 <118629689+Ritik-Kumar01@users.noreply.github.com>
2025-01-31 10:00:23 +0530

    Time: 0 ms (100%), Space: 113.9 MB (12.41%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -1,32 +1,52 @@
 class Solution {
 public:
     double findMaxAverage(vector<int>& nums, int k) {
-       int sum = 0;
-       for(int i=0;i<k;i++){
-        sum += nums[i];
-       }
-
-       int maxsum = sum;
-
-       int startIndex = 0;
-       int endIndex = k;
-
-    //    while(endIndex<nums.size()){
-              
-    //        sum -=nums[startIndex];
-    //        startIndex++;
-
-    //        sum +=nums[endIndex];
-    //        endIndex++;
-
-    //        maxsum = max(maxsum,sum);   
+    //    int sum = 0;
+    //    for(int i=0;i<k;i++){
+    //     sum += nums[i];
     //    }
 
-    for (int i = k; i < nums.size(); i++) {
-            sum = sum - nums[i - k] + nums[i];
-            maxsum = max(maxsum, sum);
-        }
+    //    int maxsum = sum;
 
-     return (double) maxsum/k;
+    //    int startIndex = 0;
+    //    int endIndex = k;
+
+    // //    while(endIndex<nums.size()){
+              
+    // //        sum -=nums[startIndex];
+    // //        startIndex++;
+
+    // //        sum +=nums[endIndex];
+    // //        endIndex++;
+
+    // //        maxsum = max(maxsum,sum);   
+    // //    }
+
+    // for (int i = k; i < nums.size(); i++) {
+    //         sum = sum - nums[i - k] + nums[i];
+    //         maxsum = max(maxsum, sum);
+    //     }
+    //  return (double) maxsum/k;
+    
+
+
+    int sum = 0 ;
+    for(int i=0; i<k;i++){
+        sum += nums[i];
+    }
+    int maxsum = sum;
+    int startIndex = 0 ;
+    int endIndex = k;
+
+    while(endIndex<nums.size()){
+        sum -= nums[startIndex];
+        startIndex++;
+
+        sum += nums[endIndex];
+        endIndex++;
+
+        maxsum = max(maxsum,sum);
+    }
+     return (double) (maxsum)/k;
     }
 };
