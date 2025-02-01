757ece83d465faf7c6abe461c2ef52209148940a
Ritik-Kumar01 <118629689+Ritik-Kumar01@users.noreply.github.com>
2025-02-01 16:33:24 +0530

    Time: 0 ms (100%), Space: 7.7 MB (87.41%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -20,21 +20,38 @@ public:
 //     }
    
     int climbStairs(int n) {
-        // memset(t,-1,sizeof(t));
-        // return solve(n);
+        // memset(t,-1,sizeof(t));          // Memorization appraoch   (T.C. - o(n) but space complexcity is also o(n) using array)
+        // return solve(n);                 // simple recursive approach  (T.c. - 2**n)
+        
+        //bottom - up dynamic programming approach
 
-        if( n==1 || n==2 || n==3){
+        // if( n==1 || n==2 || n==3){
+        //     return n;
+        // }
+
+        //  vector<int> t(n+1);
+        //  t[0] = 0;
+        //  t[1] = 1;
+        //  t[2] = 2;
+
+        //  for(int i=3; i<=n;i++){
+        //     t[i] = t[i-1]+t[i-2]; 
+        //  }
+        //  return t[n];
+
+
+
+        if(n==1 || n==2){
             return n;
         }
 
-         vector<int> t(n+1);
-         t[0] = 0;
-         t[1] = 1;
-         t[2] = 2;
+        int first=1 , second = 2, result;
 
-         for(int i=3; i<=n;i++){
-            t[i] = t[i-1]+t[i-2]; 
-         }
-         return t[n];
+        for(int i=3; i<=n;i++){
+            result = first+second;
+            first = second;
+            second = result;
+        }
+        return result;
     }
 };
