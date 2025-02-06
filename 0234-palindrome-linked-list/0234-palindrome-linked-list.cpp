306d0ee8045da613bc4a779288f42e6788cf2549
Ritik-Kumar01 <118629689+Ritik-Kumar01@users.noreply.github.com>
2025-02-06 11:09:17 +0530

    Time: 14 ms (11.18%), Space: 120.4 MB (58.65%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -8,27 +8,77 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// Fisrst approch - Brute force
+// T.C. - o(n) and space complexity also o(n) bcz we have used an extra vector.
+
+// class Solution {
+// public:
+//     bool isPalindrome(ListNode* head) {
+        
+//         vector<int> result;
+
+//         ListNode* current = head;
+//         while(current!=NULL){
+//             result.push_back(current->val);
+//             current =current->next;
+//         }
+
+//         int i = 0; int j = result.size()-1;
+//         while(i<j){
+//             if(result[i]!=result[j]){
+//                 return false;
+//             }
+//             i++;
+//             j--;
+//         }
+//         return true;
+
+//     }
+// };
+
+
+
+// 2nd approach
+
 class Solution {
+
+    ListNode* reverseLinkedList(ListNode* head){
+        if(head==NULL || head->next==NULL){
+            return head;
+        }
+
+        ListNode* last = reverseLinkedList(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        return last;
+    }
 public:
     bool isPalindrome(ListNode* head) {
+
+         if(head==NULL || head->next==NULL){
+            return true;
+        }
         
-        vector<int> result;
+       ListNode* fast = head;
+       ListNode* slow = head;
+       ListNode* prev =NULL;
 
-        ListNode* current = head;
-        while(current!=NULL){
-            result.push_back(current->val);
-            current =current->next;
+       while(fast!=NULL && fast->next!=NULL){
+          prev = slow;
+          slow =slow->next;
+          fast = fast->next->next;
+       }
+
+       ListNode* tail=reverseLinkedList(slow);
+       prev->next =NULL;
+
+       while(tail!=NULL && head!=NULL){
+        if(tail->val!=head->val){
+            return false;
         }
-
-        int i = 0; int j = result.size()-1;
-        while(i<j){
-            if(result[i]!=result[j]){
-                return false;
-            }
-            i++;
-            j--;
-        }
-        return true;
-
+        tail =tail->next;
+        head = head->next;
+       }
+       return true;
     }
 };
