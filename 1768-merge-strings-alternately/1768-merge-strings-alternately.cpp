7f12519ef7d171f39f0dc968a650d1961d322b4c
Ritik-Kumar01 <118629689+Ritik-Kumar01@users.noreply.github.com>
2025-02-07 11:27:33 +0530

    Time: 0 ms (100%), Space: 8.5 MB (63.14%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -2,17 +2,32 @@ class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
 
-     string result ="";
-     int i = 0;
-     while(i<word1.length() || i<word2.length()){
-        if(i<word1.length()){
-            result += word1[i];
+    //  string result ="";
+    //  int i = 0;
+    //  while(i<word1.length() || i<word2.length()){
+    //     if(i<word1.length()){
+    //         result += word1[i];
+    //     }
+    //     if(i<word2.length()){
+    //         result += word2[i];
+    //     }
+    //     i++;
+    //  }   
+    //  return result;
+
+
+    string result = "";
+    int i = 0;
+
+    while(word1.length()>i || word2.length()>i){
+        if(word1.length()>i){
+            result +=word1[i];
         }
-        if(i<word2.length()){
+        if(word2.length()>i){
             result += word2[i];
         }
         i++;
-     }   
-     return result;
+    }
+    return result;
     }
 };
