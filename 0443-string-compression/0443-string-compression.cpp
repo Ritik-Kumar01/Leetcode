32244fe573c2769e9440f4e0491b937fa8830e2d
Ritik-Kumar01 <118629689+Ritik-Kumar01@users.noreply.github.com>
2025-02-09 00:48:36 +0530

    Time: 0 ms (100%), Space: 13.7 MB (73.77%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -1,30 +1,57 @@
 class Solution {
 public:
-    int compress(vector<char>& a) {
-        int n = a.size();
-        int idx=0;
-        for(int i=0;i<n;i++)
-        {
-            int count=0;
-            char ch=a[i];
-            while(i<n &&a[i]==ch)
-            {
+    int compress(vector<char>& c) {
+        int n = c.size();
+
+        int index = 0 ;
+        int i = 0 ;
+        while(i<n){
+            char curr_char = c[i];
+        int count = 0 ;
+           //count duplicates
+            while(i<n && curr_char==c[i]){
                 count++;
                 i++;
             }
+            // now do the assinging work
 
-                a[idx++]=ch;
-                if(count>1)
-                {
-                for(char c:to_string(count))
-                {
-                    a[idx++]=c;
+            c[index] = curr_char;
+            index++;
+
+            if(count>1){
+                string count_str = to_string(count);
+
+                for(char ch:count_str){
+                    c[index] = ch;
+                    index++;
                 }
             }
-            i--;
+
         }
-        a.resize(idx);
-        return idx;
+        return index;
+        // int idx=0;
+        // for(int i=0;i<n;i++)
+        // {
+        //     int count=0;
+        //     char ch=a[i];
+        //     while(i<n &&a[i]==ch)
+        //     {
+        //         count++;
+        //         i++;
+        //     }
+
+        //         a[idx++]=ch;
+        //         if(count>1)
+        //         {
+        //         for(char c:to_string(count))
+        //         {
+        //             a[idx++]=c;
+        //         }
+        //     }
+        //     i--;
+        // }
+        // a.resize(idx);
+        // return idx;
     }
 };
 
