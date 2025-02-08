892245e5c3bdff36baf657e6ce8a21ad95124326
Ritik-Kumar01 <118629689+Ritik-Kumar01@users.noreply.github.com>
2025-02-08 23:44:36 +0530

    Time: 8 ms (8.96%), Space: 115.6 MB (53.29%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -18,5 +18,18 @@ public:
             }
         }
         return false;
+
+
+    //     int n = nums.size();
+    // for (int i = 0; i < n-2; i++) {
+    //     for (int j = i + 1; j < n -1; j++) {
+    //         for (int k = j + 1; k < n; k++) {
+    //             if (nums[i] < nums[j] && nums[j] < nums[k]) {
+    //                 return true;
+    //             }
+    //         }
+    //     }
+    // }
+    // return false;
     }
 };
