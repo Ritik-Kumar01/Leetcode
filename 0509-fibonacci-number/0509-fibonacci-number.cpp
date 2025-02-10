9edaf88f00af19d9b61d3e320c2615cabf61c297
Ritik-Kumar01 <118629689+Ritik-Kumar01@users.noreply.github.com>
2025-02-10 16:20:57 +0530

    Time: 0 ms (100%), Space: 8.1 MB (17.07%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,11 +1,26 @@
 class Solution {
 public:
+
+   int solve(vector<int> &dp, int n){
+    if(n<=1){
+        return n;
+    }
+    if(dp[n]!=-1){
+        return dp[n];
+    }
+
+    return  dp[n]=solve(dp,n-1)+solve(dp,n-2);
+   }
     int fib(int n) {
         if(n<=1){
             return n;
         }
 
-        return fib(n-1) + fib(n-2);
+        // return fib(n-1) + fib(n-2);
+
+        vector<int> dp(n+1,-1);
+
+        return solve(dp,n);
         
     }
 };
