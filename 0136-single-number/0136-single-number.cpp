b74c69f0093f6020cfd15a7abc0d1e08c1accdfb
Ritik-Kumar01 <118629689+Ritik-Kumar01@users.noreply.github.com>
2025-02-13 01:49:42 +0530

    Time: 2 ms (29.29%), Space: 20.6 MB (59.08%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+    //      int result = 0;
+    // for (int num : nums) {
+    //     result ^= num;  // XOR all elements
+    // }
+    // return result;
+
+
+    sort(nums.begin(),nums.end());
+    int n = nums.size();
+
+    for(int i=0;i<n-1;i+=2){
+        if(nums[i]!=nums[i+1]){
+            return nums[i];
+        }
+    }
+    return nums[n-1];
+    }
+};
