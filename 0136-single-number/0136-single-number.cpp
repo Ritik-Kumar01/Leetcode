460bc09e71924088d27af675376ba3088b2e3a69
Ritik-Kumar01 <118629689+Ritik-Kumar01@users.noreply.github.com>
2025-02-13 02:04:34 +0530

    Time: 11 ms (16.8%), Space: 24 MB (7.09%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -9,14 +9,27 @@ public:
     // return result;
 
 
-    sort(nums.begin(),nums.end());
-    int n = nums.size();
+    // sort(nums.begin(),nums.end());
+    // int n = nums.size();
 
-    for(int i=0;i<n-1;i+=2){
-        if(nums[i]!=nums[i+1]){
-            return nums[i];
+    // for(int i=0;i<n-1;i+=2){
+    //     if(nums[i]!=nums[i+1]){
+    //         return nums[i];
+    //     }
+    // }
+    // return nums[n-1];
+
+    unordered_map<int,int> freq;
+
+    for(int num : nums){
+        freq[num]++;
+    }
+
+    for(auto pair : freq){
+        if(pair.second==1){
+            return pair.first;
         }
     }
-    return nums[n-1];
+    return -1;
     }
 };
