4fcfa7ee9e1a31b48688104893fc9a9d1018290f
Ritik-Kumar01 <118629689+Ritik-Kumar01@users.noreply.github.com>
2025-02-25 15:21:26 +0530

    Time: 3 ms (35.52%), Space: 63 MB (42.29%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,28 +1,65 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int n = height.size();
+        // int n = height.size();
 
-        int i = 0 , j = n-1;
+        // int i = 0 , j = n-1;
 
-        int maxamount = 0;
+        // int maxamount = 0;
 
-        while(i<j){
-            int width = j - i;
+        // while(i<j){
+        //     int width = j - i;
 
-            int h = min(height[i],height[j]);
+        //     int h = min(height[i],height[j]);
 
-            int area = width * h;
+        //     int area = width * h;
 
-            maxamount = max(maxamount,area);
+        //     maxamount = max(maxamount,area);
 
-            if(height[i]>height[j]){
-                j--;
-            }
-            else{
-                i++;
-            }
-        }
-        return maxamount;
+        //     if(height[i]>height[j]){
+        //         j--;
+        //     }
+        //     else{
+        //         i++;
+        //     }
+        // }
+        // return maxamount;
+    //     int heightt ;
+    //     int width = 1;
+    //     int area = 1;
+    //     int maxarea=0;
+    //     for(int i = 0 ; i<height.size();i++){
+    //         for(int j=i+1; j<height.size();j++){
+    //              heightt= min(height[i],height[j]);
+    //             width = j - i;
+    //              area = heightt*width;
+    //              maxarea = max(maxarea,area);
+    //         }
+    //     }
+    //     return maxarea;
+    // }
+
+
+    int i = 0 ;
+    int j = height.size()-1;
+    int maxarea  = 0;
+
+    while(i<j){
+        int width = j - i;
+
+        int heightt = min(height[i],height[j]);
+
+        int area = heightt * width;
+
+         maxarea = max(maxarea,area);
+
+         if(height[i]>height[j]){
+            j--;
+         }
+         else{
+            i++;
+         }
+    }
+         return maxarea;
     }
 };
