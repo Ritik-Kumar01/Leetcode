39cc74456119bfc4e68124f4b3c2cc3191dccfda
Ritik-Kumar01 <118629689+Ritik-Kumar01@users.noreply.github.com>
2025-03-06 11:48:30 +0530

    Time: 0 ms (100%), Space: 26.8 MB (74.89%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
-    long long coloredCells(int n) {
-        return 1LL * n * n + (n - 1LL) * (n - 1);
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int size = n * n;
+        vector<int> freq(size + 1, 0);
+        int repeated = -1, missing = -1;
+
+        for (const auto& row : grid) {
+            for (int num : row) {
+                freq[num]++;
+            }
+        }
+
+        for (int num = 1; num <= size; num++) {
+            if (freq[num] == 2) repeated = num;
+            if (freq[num] == 0) missing = num;
+        }
+
+        return {repeated, missing};
     }
 };
