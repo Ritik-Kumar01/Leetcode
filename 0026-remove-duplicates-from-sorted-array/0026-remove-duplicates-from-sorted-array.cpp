ff43e3f766683ca0bbdd250ecdf67c89d99e7725
Ritik-Kumar01 <118629689+Ritik-Kumar01@users.noreply.github.com>
2025-03-08 13:06:51 +0530

    Time: 0 ms (100%), Space: 22.6 MB (50.66%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,31 +1,17 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        // int n = nums.size();
+        int n = nums.size();
         
-        // int i = 0, j =1;
+        int i = 0, j =1;
 
-        // while(j<n){
-        //     if(nums[i]!=nums[j]){   // got a unique element
-        //         i++;
-        //         nums[i] = nums[j];
-        //     }
-        //     j++;
-        // }
-        // return i+1;   // to find the unique element
-
-
-          unordered_map<int, bool> seen;
-    vector<int> result;
-
-    for (int num : nums) {
-        if (seen.find(num) == seen.end()) {  // If not found in map, it's unique
-            result.push_back(num);
-            seen[num] = true;  // Mark as seen
+        while(j<n){
+            if(nums[i]!=nums[j]){   // got a unique element
+                i++;
+                nums[i] = nums[j];
+            }
+            j++;
         }
-    }
-
-    nums = result;  // Copy result back to nums
-    return result.size();  // Return count of unique elements
+        return i+1;   // to find the unique element
     }
 };
