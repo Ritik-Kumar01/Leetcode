0e72f84c3412f0cbe8b208088168a4d544ce3c96
Ritik-Kumar01 <118629689+Ritik-Kumar01@users.noreply.github.com>
2025-03-09 00:04:56 +0530

    Time: 0 ms (100%), Space: 8.5 MB (31.01%) - LeetHub

--- 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.cpp
+++ 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.cpp
@@ -1,19 +1,44 @@
 class Solution {
 public:
     int minimumRecolors(string blocks, int k) {
-        int n = blocks.length();
-        int result = k; // Initialize result with the maximum possible value
+        // int n = blocks.length();
+        // int result = k; // Initialize result with the maximum possible value
         
-        for (int i = 0; i <= n - k; i++) { // Ensure valid range
-            int W = 0;
-            for (int j = i; j < i + k; j++) { // Iterate over the k-length window
-                if (blocks[j] == 'W') {
-                    W++;
-                }
+        // for (int i = 0; i <= n - k; i++) { // Ensure valid range
+        //     int W = 0;
+        //     for (int j = i; j < i + k; j++) { // Iterate over the k-length window
+        //         if (blocks[j] == 'W') {
+        //             W++;
+        //         }
+        //     }
+        //     result = min(result, W); // Update the minimum number of recolors
+        // }
+        
+        // return result;
+
+        int n = blocks.size();
+        int i = 0; 
+        int j = 0;
+
+        int ops = k;
+        int W = 0;
+
+        while(j<n){
+            if(blocks[j]=='W'){
+                W++;
             }
-            result = min(result, W); // Update the minimum number of recolors
+
+            if(j-i+1 == k){
+                ops = min(ops,W);
+
+                if(blocks[i] == 'W'){
+                    W--;
+                }
+                i++;
+            }
+            j++;
+
         }
-        
-        return result;
+        return ops;
     }
 };
