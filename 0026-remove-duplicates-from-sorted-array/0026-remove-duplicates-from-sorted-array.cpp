a874870f07617296c969351f670bfa5122aab04e
Ritik-Kumar01 <118629689+Ritik-Kumar01@users.noreply.github.com>
2025-03-08 13:06:55 +0530

    Time: 0 ms (100%), Space: 22.9 MB (10.47%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -3,15 +3,26 @@ public:
     int removeDuplicates(vector<int>& nums) {
         int n = nums.size();
         
-        int i = 0, j =1;
+        // int i = 0, j =1;
 
-        while(j<n){
-            if(nums[i]!=nums[j]){   // got a unique element
-                i++;
-                nums[i] = nums[j];
+        // while(j<n){
+        //     if(nums[i]!=nums[j]){   // got a unique element
+        //         i++;
+        //         nums[i] = nums[j];
+        //     }
+        //     j++;
+        // cout<<nums[i]<<" ";
+        // }
+        // return i+1;   // to find the unique element
+
+        vector<int>vec;
+
+        for(int num : nums){
+            if(vec.empty() || vec.back()!=num){
+                vec.push_back(num);
             }
-            j++;
         }
-        return i+1;   // to find the unique element
+                nums = vec;
+        return vec.size();
     }
 };
