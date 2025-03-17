83154dc315071876ac6751e8026945828fde88a6
Ritik-Kumar01 <118629689+Ritik-Kumar01@users.noreply.github.com>
2025-03-17 09:51:13 +0530

    Time: 1 ms (55.8%), Space: 312 MB (83.61%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -16,39 +16,39 @@ public:
             return NULL;
         }
 
-        // ListNode* prevslow = NULL;
-        // ListNode* slow = head;
-        // ListNode* fast = head;
+        ListNode* prevslow = NULL;
+        ListNode* slow = head;
+        ListNode* fast = head;
 
-        // while(fast!=NULL && fast->next!=NULL){
-        //     prevslow = slow;
+        while(fast!=NULL && fast->next!=NULL){
+            prevslow = slow;
 
-        //     slow = slow -> next;
-        //     fast = fast->next ->next;
+            slow = slow -> next;
+            fast = fast->next ->next;
+        }
+
+        prevslow ->next = slow -> next;
+        // delete(slow);
+        return head;
+
+
+        // ListNode* temp = head;
+        
+        // int length = 0;
+        // while(temp!=NULL){
+        //     length++;
+        //     temp = temp->next;
         // }
 
-        // prev ->next = slow -> next;
-        // delete(slow);
+        // int median = length/2;
+
+        // temp = head;
+        // for(int i = 0; i<median -1;i++){
+        //     temp= temp->next;
+        // }
+        
+        // temp -> next = temp ->next ->next;
         // return head;
-
-
-        ListNode* temp = head;
-        
-        int length = 0;
-        while(temp!=NULL){
-            length++;
-            temp = temp->next;
-        }
-
-        int median = length/2;
-
-        temp = head;
-        for(int i = 0; i<median -1;i++){
-            temp= temp->next;
-        }
-        
-        temp -> next = temp ->next ->next;
-        return head;
         
     }
 };
