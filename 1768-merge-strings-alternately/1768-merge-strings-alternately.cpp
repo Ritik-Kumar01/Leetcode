b10999e259c6563f488a762c53cb405d9f5c553f
Ritik-Kumar01 <118629689+Ritik-Kumar01@users.noreply.github.com>
2025-04-01 12:40:32 +0530

    Time: 3 ms (40.3%), Space: 8.6 MB (31.01%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -16,18 +16,32 @@ public:
     //  return result;
 
 
+    // string result = "";
+    // int i = 0;
+
+    // while(word1.length()>i || word2.length()>i){
+    //     if(word1.length()>i){
+    //         result +=word1[i];
+    //     }
+    //     if(word2.length()>i){
+    //         result += word2[i];
+    //     }
+    //     i++;
+    // }
+    // return result;
+
+
     string result = "";
     int i = 0;
-
-    while(word1.length()>i || word2.length()>i){
-        if(word1.length()>i){
+     while(i<word1.length() || i<word2.length()){
+        if(i<word1.length()){
             result +=word1[i];
         }
-        if(word2.length()>i){
-            result += word2[i];
+        if(i<word2.length()){
+            result +=word2[i];
         }
         i++;
-    }
-    return result;
+     }
+     return result;
     }
 };
