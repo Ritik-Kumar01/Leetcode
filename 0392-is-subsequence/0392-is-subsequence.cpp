2f921f14d103ea1728061dfb2d204ccc67a5f9ad
Ritik-Kumar01 <118629689+Ritik-Kumar01@users.noreply.github.com>
2025-05-21 17:49:25 +0530

    Time: 0 ms (100%), Space: 8.5 MB (90.52%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -34,10 +34,26 @@ public:
 
 
 
-        int i=0;
-        int j=0;
+        // int i=0;
+        // int j=0;
 
-        while(i<s.size() && j<t.size()){
+        // while(i<s.size() && j<t.size()){
+        //     if(s[i]==t[j]){
+        //         i++;
+        //         j++;
+        //     }
+        //     else{
+        //         j++;
+        //     }
+        // }
+        // return (i==s.size());
+
+
+
+        int i  =0 ;
+        int j = 0;
+
+        while(j<t.size()){
             if(s[i]==t[j]){
                 i++;
                 j++;
