a9a8314a5d0845ca05c721f63039e1f670637192
Ritik-Kumar01 <118629689+Ritik-Kumar01@users.noreply.github.com>
2025-05-30 11:43:01 +0530

    Time: 10 ms (37.73%), Space: 13.3 MB (18.37%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,28 +1,53 @@
 class Solution {
 public:
     int maxVowels(string s, int k) {
-       int maxVowels = 0;
-        int windowVowels = 0;
-        unordered_set<char> vowels = {'a', 'e', 'i', 'o', 'u'};
+    //    int maxVowels = 0;
+    //     int windowVowels = 0;
+    //     unordered_set<char> vowels = {'a', 'e', 'i', 'o', 'u'};
 
-        for (int i = 0; i < k; i++) {
-            if (vowels.count(s[i])) {
-                windowVowels++;
-            }
-        }
-        maxVowels = windowVowels;
+    //     for (int i = 0; i < k; i++) {
+    //         if (vowels.count(s[i])) {
+    //             windowVowels++;
+    //         }
+    //     }
+    //     maxVowels = windowVowels;
 
      
-        for (int i = k; i < s.length(); i++) {
-            if (vowels.count(s[i - k])) {
-                windowVowels--;
-            }
-            if (vowels.count(s[i])) {
-                windowVowels++;
-            }
-            maxVowels = max(maxVowels, windowVowels);
+    //     for (int i = k; i < s.length(); i++) {
+    //         if (vowels.count(s[i - k])) {
+    //             windowVowels--;
+    //         }
+    //         if (vowels.count(s[i])) {
+    //             windowVowels++;
+    //         }
+    //         maxVowels = max(maxVowels, windowVowels);
+    //     }
+
+    //     return maxVowels;
+
+    int windowvowel = 0;
+    int maxvowel = 0;
+
+    unordered_set<char> vowels = {'a','e','i','o','u'};
+
+    for(int i=0;i<k;i++){
+        if(vowels.count(s[i])){
+            windowvowel++;
+        }
+    }
+
+    maxvowel = windowvowel;
+
+    for(int i=k;i<s.size();i++){
+        if(vowels.count(s[i-k])){
+            windowvowel--;
+        }
+        if(vowels.count(s[i])){
+            windowvowel++;
         }
 
-        return maxVowels;
+        maxvowel = max(maxvowel,windowvowel);
+    }
+    return maxvowel;
     }
 };
