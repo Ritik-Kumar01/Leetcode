1e8bd5227d0ed818389822bf91a4c8d1a17d8053
Ritik-Kumar01 <118629689+Ritik-Kumar01@users.noreply.github.com>
2025-06-12 22:28:08 +0530

    Time: 3 ms (28.9%), Space: 97.5 MB (26.45%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -3,28 +3,47 @@ public:
     int maxProfit(vector<int>& prices) {
         // int maxsum = INT_MIN;
         // int sum = 0;
-        // bool check = true;
+       
         // for(int i = 0 ; i<prices.size();i++){
-        //     sum = 0;
+     
         //     for(int j = i+1;j<prices.size();j++){
         //         if(prices[i]<prices[j]){
-        //              sum = prices[j] - prices[i];
-        //              maxsum = max(sum,maxsum);
-        //              check =false;
+        //             sum = max(sum,prices[j]-prices[i]);
+        //        
         //         }
         //     }
         // }
-        //         if(check==true){
-        //             return 0;
-        //         }
-        // return maxsum;
+        // return sum;
 
-        int minprice = INT_MAX;
+        // int minprice = INT_MAX;
+        // int maxprofit = 0;
+
+        // for(int price : prices){
+        //     minprice = min(minprice,price);
+        //     maxprofit = max(maxprofit,price-minprice);
+        // }
+        // return maxprofit;
+
+        // int buy = prices[0];
+        // int profit = 0;
+
+        // for(int i = 1; i<prices.size();i++){
+        //     if(prices[i]<buy){
+        //         buy = prices[i];
+        //     }
+        //     profit = max(profit,prices[i]-buy);
+        // }
+        // return profit;
+
+
+        int price = prices[0];
+
         int maxprofit = 0;
 
-        for(int &price : prices){
-            minprice = min(minprice,price);
-            maxprofit = max(maxprofit,price-minprice);
+        for(int i=1; i<prices.size();i++){
+            int mini = min(price,prices[i]);
+            price = mini;
+            maxprofit = max(maxprofit,prices[i]-mini);
         }
         return maxprofit;
     }
