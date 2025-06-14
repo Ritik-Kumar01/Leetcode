a8530ef1f178405f537363923b650473277c5eba
Ritik-Kumar01 <118629689+Ritik-Kumar01@users.noreply.github.com>
2025-06-15 02:18:54 +0530

    Time: 0 ms (100%), Space: 14.2 MB (48.84%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -4,19 +4,32 @@ public:
 
         // int min = *min_element(nums.begin(),nums.end());
         // return min;
-        int n=nums.size();
-        int a=0;
-        int b=0;
-        int c=INT_MAX;
-            if(nums.size()==1){
-                return nums[0];
-            }
-        for(int i=0;i<=n/2;i++){
-            a=nums[i];
-            b=nums[n-i-1];
-            c=min(c,min(a,b));
+        // int n=nums.size();
+        // int a=0;
+        // int b=0;
+        // int c=INT_MAX;
+        // if()
+            
+        // for(int i=0;i<n/2;i++){
+        //     a=nums[i];
+        //     b=nums[n-i-1];
+        //     c=min(c,min(a,b));
 
-        }return c;
+        // }return c;
         
+
+         int left = 0, right = nums.size() - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return nums[left];
     }
 };
