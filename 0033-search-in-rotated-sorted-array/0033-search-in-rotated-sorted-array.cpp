be86301a164c9afca2a072385af1f6d3bb3aa6e5
Ritik-Kumar01 <118629689+Ritik-Kumar01@users.noreply.github.com>
2025-06-15 23:21:31 +0530

    Time: 0 ms (100%), Space: 15.2 MB (33.17%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,56 +1,47 @@
 class Solution {
 public:
+    int pivot(vector<int>& nums, int n) {
+        int left = 0;
+        int right = nums.size() - 1;
 
-   int Findpivot(vector<int> &nums,int n){
-    int l=0;
-    int r = n-1;
-
-    while(l<r){
-        int mid = l +(r-l)/2;
-
-        if(nums[mid]>nums[r]){
-           l = mid+1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
         }
-        else{
-            r = mid;
-        }
-        
+        return left;
     }
-        return r;
-   }
 
-   int BinarySearch(int l, int r, vector<int>&nums,int target){
-    int idx = -1;
+    int binarysearch(int l, int r, vector<int>& nums, int target) {
+        int idx = -1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
 
-    while(l<=r){
-        int mid = l +(r-l)/2;
-
-        if(nums[mid]==target){
-            idx = mid;
-            break;
+            if (nums[mid] == target) {
+                idx = mid;
+                break;
+            } else if (nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
         }
-        else if(nums[mid]<target){
-            l = mid+1;
-        }
-        else{
-            r = mid - 1;
-        }
+        return idx;
     }
-    return idx;
-   }
 
     int search(vector<int>& nums, int target) {
-
         int n = nums.size();
+        int pivot_index = pivot(nums, n);
 
-        int pivot_index = Findpivot(nums,n);
-
-        int idx = BinarySearch(0,pivot_index-1,nums,target);// left side of pivot index
-        if(idx!=-1){
+        int idx = binarysearch(0, pivot_index - 1, nums, target);
+        if (idx != -1) {
             return idx;
         }
 
-         idx = BinarySearch(pivot_index,n-1,nums,target);  // right side including pivot index
+        idx = binarysearch(pivot_index, nums.size() - 1, nums, target);
         return idx;
     }
 };
