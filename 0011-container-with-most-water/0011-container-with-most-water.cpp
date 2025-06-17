b88c765bb3d7c420c7ea42406d9ca9b72badf9af
Ritik-Kumar01 <118629689+Ritik-Kumar01@users.noreply.github.com>
2025-06-17 10:28:06 +0530

    Time: 0 ms (100%), Space: 63 MB (12.6%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,109 +1,28 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        // int n = height.size();
+        int start = 0 ;
+        int end = height.size()-1;
+        int maxarea = 0;
 
-        // int i = 0 , j = n-1;
+        while(start<end){
+            int length = end-start;
 
-        // int maxamount = 0;
+            int breadth = min(height[start],height[end]);
 
-        // while(i<j){
-        //     int width = j - i;
+            int area = length * breadth;
 
-        //     int h = min(height[i],height[j]);
-
-        //     int area = width * h;
-
-        //     maxamount = max(maxamount,area);
-
-        //     if(height[i]>height[j]){
-        //         j--;
-        //     }
-        //     else{
-        //         i++;
-        //     }
-        // }
-        // return maxamount;
-    //     int heightt ;
-    //     int width = 1;
-    //     int area = 1;
-    //     int maxarea=0;
-    //     for(int i = 0 ; i<height.size();i++){
-    //         for(int j=i+1; j<height.size();j++){
-    //              heightt= min(height[i],height[j]);
-    //             width = j - i;
-    //              area = heightt*width;
-    //              maxarea = max(maxarea,area);
-    //         }
-    //     }
-    //     return maxarea;
-    // }
+            maxarea = max(maxarea, area);
 
 
-    // int i = 0 ;
-    // int j = height.size()-1;
-    // int maxarea  = 0;
-
-    // while(i<j){
-    //     int width = j - i;
-
-    //     int heightt = min(height[i],height[j]);
-
-    //     int area = heightt * width;
-
-    //      maxarea = max(maxarea,area);
-
-    //      if(height[i]>height[j]){
-    //         j--;
-    //      }
-    //      else{
-    //         i++;
-    //      }
-    // }
-    //      return maxarea;
-
-
-    // int i = 0; 
-    // int maxarea = 0;
-    // int j = height.size()-1;
-
-    // while(i<j){
-    //     int width  = j -  i ;
-    //     int heightt = min(height[i],height[j]);
-
-    //     int area = width * heightt;
-
-    //     maxarea = max(maxarea,area);
-
-    //     if(height[i]>height[j]){
-    //         j--;
-    //     }
-    //     else{
-    //         i++;
-    //     }
-    // }
-    // return maxarea;
-
-
-    int i = 0 ; int j=height.size()-1;
-    int maxarea = 0;
-
-    while(i<j){
-        int width = j - i;
-
-        int heightt = min(height[i],height[j]);
-
-        int area = width * heightt;
-
-        maxarea = max(area,maxarea);
-
-        if(height[i]>height[j]){
-            j--;
+            if(height[start]>height[end]){
+                end--;
+            }
+            else{
+                start++;
+            }
         }
-        else{
-            i++;
-        }
-    }
-    return maxarea;
+        return maxarea;
+        
     }
 };
