e95cb9109dbec695d7200421b505059cc7853110
Ritik-Kumar01 <118629689+Ritik-Kumar01@users.noreply.github.com>
2025-06-20 13:39:21 +0530

    Time: 0 ms (100%), Space: 10.4 MB (100%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -18,11 +18,33 @@ public:
     // return vec;
 
 
-    vector<int> vec(n+1);
+    // vector<int> vec(n+1);
 
-    for(int i=0;i<=n;i++){
-        vec[i] = __builtin_popcount(i);
+    // for(int i=0;i<=n;i++){
+    //     vec[i] = __builtin_popcount(i);
+    // }
+    // return  vec;
+
+
+    vector<int> result(n+1);
+
+    if(n==0){
+        return result;
     }
-    return  vec;
+
+    result[0] = 0;
+
+    for(int i=1; i<=n;i++){
+        if(i%2!=0){
+            result[i] = result[i/2]+1;
+        }
+        else{
+            result[i] = result[i/2];
+        }
+    }
+    return result;
+
+
+
     }
 };
