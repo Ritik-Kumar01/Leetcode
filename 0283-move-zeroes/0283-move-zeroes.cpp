ce61c635bed29f25badbc7dc378276361c2915b8
Ritik-Kumar01 <118629689+Ritik-Kumar01@users.noreply.github.com>
2025-06-23 16:58:03 +0530

    Time: 7 ms (11.66%), Space: 25 MB (5.76%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,60 +1,31 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        vector<int>zero;
+        vector<int>nonzero;
+
+        for(int num:nums){
+            if(num!=0){
+                nonzero.push_back(num);
+            }
+        }
+
+        for(int num:nums){
+            if(num==0){
+                zero.push_back(num);
+            }
+        }
+         int i = 0;
+        for(int non:nonzero){
+            nums[i]= non;
+            i++;
+            
+        }
+
+        for(int ze : zero){
+            nums[i] = ze;
+            i++;
+        }
         
-
-    //   int insertposition = 0;
-    //   for(int i=0; i<nums.size();i++){
-    //     if(nums[i]!=0){
-    //         nums[insertposition++] = nums[i];
-    //     }
-    //   }  
-
-    //   while(insertposition<nums.size()){
-    //     nums[insertposition++]=0;
-    //   }
-
-
-    //  vector<int> nonzero;
-    //  vector<int> zero;
-
-    //  for(int num : nums){
-    //   if(num != 0){
-    //     nonzero.push_back(num);
-    //   }
-
-    //   else{
-    //     zero.push_back(num);
-    //   }
-    //  }
-
-    // int index = 0;
-    // for(int num : nonzero){
-    //     nums[index++] = num;
-    // }
-    // for(int num : zero){
-    //     nums[index++] = num;
-    // }
-
-    vector<int> zero;
-    vector<int> nonzero;
-
-    for(int num : nums){
-        if(num!=0){
-            nonzero.push_back(num);
-        }
-        else{
-            zero.push_back(num);
-        }
-    }
-    
-    int index = 0;
-    for(int num : nonzero){
-        nums[index++] = num;
-    }
-
-    for(int num : zero){
-        nums[index++] = num;
-    }
     }
 };
