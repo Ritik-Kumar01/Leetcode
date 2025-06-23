4f1cd407f4465b4c14cb69b5eb449444745c21bf
Ritik-Kumar01 <118629689+Ritik-Kumar01@users.noreply.github.com>
2025-06-23 18:07:21 +0530

    Time: 103 ms (41.8%), Space: 69.5 MB (41.71%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -1,23 +1,41 @@
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
-         sort(nums.begin(),nums.end());
-        int left = 0 ; 
-         int right = nums.size()-1;
-          int count = 0;
-         while(left<right){
-            if(nums[left]+nums[right]>k){
-                       right--;
-            }
-            else if(nums[left]+nums[right]<k){
-                left++;
-            }
-            else if(nums[left]+nums[right]==k){
-                right--;
-                left++;
+        //  sort(nums.begin(),nums.end());
+        // int left = 0 ; 
+        //  int right = nums.size()-1;
+        //   int count = 0;
+        //  while(left<right){
+        //     if(nums[left]+nums[right]>k){
+        //                right--;
+        //     }
+        //     else if(nums[left]+nums[right]<k){
+        //         left++;
+        //     }
+        //     else if(nums[left]+nums[right]==k){
+        //         right--;
+        //         left++;
+        //         count++;
+        //     }
+        //  }
+        // return count;
+
+
+
+        unordered_map<int,int> freq;
+         int count = 0 ;
+        for(int num : nums){
+            int complement = k - num;
+
+            if(freq.find(complement)!=freq.end() && freq[complement]>0){
                 count++;
+                freq[complement]--;
             }
-         }
+           else{
+            freq[num]++;
+           }
+            
+        }
         return count;
     }
 };
