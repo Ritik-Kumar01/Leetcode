2be6e2e4ebe44e84130c80fe479c0266998b08ec
Ritik-Kumar01 <118629689+Ritik-Kumar01@users.noreply.github.com>
2025-06-28 16:49:23 +0530

    Time: 0 ms (100%), Space: 71.8 MB (16.76%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,56 +1,22 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-    //     int sum = 0;
-    //     int maxsum = INT_MIN;
-    //     for(int i = 0 ; i<nums.size();i++){
-    //         sum = 0;
-    //         for(int j=i;j<nums.size();j++){
-    //             sum = sum + nums[j];
-    //             maxsum = max(maxsum,sum);
-    //             // cout<<maxsum<<" ";
-    //         }
-    //     }
-    //    return maxsum; 
 
-    // int sum  = 0;
-    // int maxsum  = INT_MIN;
-    // for(int i = 0 ; i<nums.size();i++){
-
-    //     sum +=nums[i];
-
+        int maxsum = INT_MIN;
+        int sum = 0;
         
-    //     maxsum = max(maxsum,sum);
-    //     if(sum<0){
-    //         sum = 0;
-    //     }
-
-    // }
-    // return maxsum;
-
-    // if we need to  print the full subarray
-
-    int sum  = 0;
-    int maxsum  = INT_MIN;
-    int startindex = -1;
-    int endindex = -1;
-    int start=0;
-    for(int i = 0 ; i<nums.size();i++){
-     if(sum==0) start = i;
-        sum +=nums[i];
-
         
-       if(maxsum<sum){
-        maxsum = sum;
-        startindex = start ; endindex = i;
-       }
-        if(sum<0){
-            sum = 0;
+        for(int i = 0 ; i<nums.size();i++){
+
+            sum +=nums[i];
+
+
+            maxsum = max(maxsum,sum);
+            if(sum<0){
+                sum = 0;
+            }
         }
-
-    }
-    return maxsum;
-
-   
+        return maxsum;
+        
     }
 };
