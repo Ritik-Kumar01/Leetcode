7b0705e4ffee86557bd775d90495df2e3af61d63
Ritik-Kumar01 <118629689+Ritik-Kumar01@users.noreply.github.com>
2025-07-07 09:22:50 +0530

    Time: 24 ms (15.9%), Space: 14.4 MB (22.53%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -2,27 +2,66 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
 
+        // int left = 0;
+        // int maxlength = 0;
+        // unordered_set<int> set;
+
+        // for(int right=0;right<s.length();right++){
+        //     if(set.count(s[right])==0){
+        //         set.insert(s[right]);
+        //         maxlength = max(maxlength,right-left+1);
+        //     }
+
+        //     else{
+        //         // while(set.count(s[right])){
+        //             while(set.find(s[right])!= set.end()){
+        //             set.erase(s[left]);
+        //             left++;
+        //         }   
+        //     }
+        //     set.insert(s[right]);
+
+        // }
+        // return maxlength;
+
+
+
+        // int left = 0 ;
+        // int maxlength = 0;
+        // unordered_set<int> set;
+
+        // for(int right=0;right<s.length();right++){
+        //    if(set.count(s[right])==0){
+        //     set.insert(s[right]);
+        //     maxlength = max(maxlength,right-left+1);
+        //    }
+        //    else{
+        //     while(set.find(s[right])!=set.end()){
+        //         set.erase(s[left]);
+        //         left++;
+        //     }
+        //    }
+        //    set.insert(s[right]);
+        // }
+        // return maxlength;
+
         int left = 0;
         int maxlength = 0;
-        unordered_set<int> set;
+        unordered_set<int>set;
 
         for(int right=0;right<s.length();right++){
             if(set.count(s[right])==0){
                 set.insert(s[right]);
                 maxlength = max(maxlength,right-left+1);
             }
-
             else{
-                // while(set.count(s[right])){
-                    while(set.find(s[right])!= set.end()){
+                while(set.find(s[right])!=set.end()){
                     set.erase(s[left]);
                     left++;
-                }   
+                }
             }
             set.insert(s[right]);
-
         }
         return maxlength;
-        
     }
 };
