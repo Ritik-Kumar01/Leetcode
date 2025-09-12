29bcff60604efa2723c64af7a73647bb7a868623
Ritik-Kumar01 <118629689+Ritik-Kumar01@users.noreply.github.com>
2025-09-12 16:57:32 +0530

    Time: 9 ms (9.65%), Space: 25 MB (6.3%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,30 +1,50 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        // vector<int>zero;
+        // vector<int>nonzero;
+
+        // for(int num:nums){
+        //     if(num!=0){
+        //         nonzero.push_back(num);
+        //     }
+        // }
+
+        // for(int num:nums){
+        //     if(num==0){
+        //         zero.push_back(num);
+        //     }
+        // }
+        //  int i = 0;
+        // for(int non:nonzero){
+        //     nums[i++]= non;
+            
+            
+        // }
+
+        // for(int ze : zero){
+        //     nums[i++] = ze;
+            
+        // }
+
+
         vector<int>zero;
         vector<int>nonzero;
 
-        for(int num:nums){
-            if(num!=0){
-                nonzero.push_back(num);
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
+                nonzero.push_back(nums[i]);
             }
-        }
-
-        for(int num:nums){
-            if(num==0){
-                zero.push_back(num);
+            else{
+                zero.push_back(nums[i]);
             }
         }
          int i = 0;
-        for(int non:nonzero){
-            nums[i++]= non;
-            
-            
+        for(int num:nonzero){
+            nums[i++]= num;
         }
-
-        for(int ze : zero){
-            nums[i++] = ze;
-            
+        for(int num:zero){
+            nums[i++]=num;
         }
         
     }
