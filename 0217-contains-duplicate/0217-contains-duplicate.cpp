f2396696d5c34b97746e90f78cc963e871d28e00
RITIK KUMAR <118629689+Ritik-Kumar01@users.noreply.github.com>
2026-02-19 17:43:37 +0530

    Time: 44 ms (68.23%), Space: 90.8 MB (43.95%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,16 +1,45 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int,int> hp;
 
-        for(int i=0;i<nums.size();i++){
-            hp[nums[i]]++;
-            if(hp[nums[i]]>=2){
+
+        // unordered_map<int , int> mp;
+
+        // for(int i = 0 ; i<nums.size();i++){
+        //     if(mp.find(nums[i]) !=mp.end()){
+        //         return true;
+        //     }
+        //     else{
+        //         mp[nums[i]] = 1;
+        //     }
+        // }
+        // return false;
+
+
+
+        unordered_set<int> s;
+
+        // for(int i = 0 ; i<nums.size();i++){
+
+        //     if(s.find(nums[i])!=s.end()){
+        //               return true;
+        //     }
+        //     s.insert(nums[i]);
+        // }
+        // return false;
+        
+
+
+        unordered_set<int>st;
+
+        for(int x : nums){
+            if(st.count(x)==0){
+                st.insert(x);
+            }
+            else{
                 return true;
             }
         }
-return false;
-        
-        
+        return false;
     }
 };
