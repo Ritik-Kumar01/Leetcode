2f79c701b95b59c5218089043506f9896c115a89
RITIK KUMAR <118629689+Ritik-Kumar01@users.noreply.github.com>
2026-02-19 17:48:57 +0530

    Time: 5 ms (14.06%), Space: 28.2 MB (62.89%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,33 +1,39 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        // int el =0;
-        // int count = 0;
-        // for(int i=0;i<nums.size();i++){
-        //     if(count==0){
-        //         el = nums[i];
-        //     }
-        //     if(el == nums[i]) count++;
-        //     else count--;
+
+        // unordered_map<int,int>count;
+        // int majorityele = 0;
+
+        // for(int i =0;i<nums.size();i++){
+        //      count[nums[i]]++;
+        //      majorityele = max(majorityele, count[nums[i]]);
         // }
-        // return el;
 
-        unordered_map<int,int> count;
-        int majorityele=0;
+        // int maxans = 0;
 
-        for(int i = 0 ; i<nums.size();i++){
-            count[nums[i]]++;
+        // for(int i=0;i<nums.size();i++){
+        //     if(majorityele == count[nums[i]]){
+        //         maxans=nums[i];
+        //         break;
+        //     }
+        // }
+        // return maxans;
 
-          majorityele = max(majorityele, count[nums[i]]);
+       unordered_map<int,int>mp;
+       int maxelement = INT_MIN;
+
+       for(int i=0;i<nums.size();i++){
+        mp[nums[i]]++;
+        maxelement = max(maxelement,mp[nums[i]]);
+       }
+     int maxans;
+       for(int i = 0 ; i<nums.size();i++){
+        if(maxelement==mp[nums[i]]){
+            maxans = nums[i];
+            break;
         }
-       
-        int maxans = 0 ;
-        for(int i=0;i<nums.size();i++){
-            if(majorityele==count[nums[i]]){
-                maxans = nums[i];
-            }
-        }
-        return maxans;
-        
+       }
+       return maxans;
     }
 };
