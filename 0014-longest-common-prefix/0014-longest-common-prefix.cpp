44dce19103994abe17099535c4f955a9d4d93397
RITIK KUMAR <118629689+Ritik-Kumar01@users.noreply.github.com>
2026-02-19 17:19:34 +0530

    Time: 0 ms (100%), Space: 11.9 MB (63.01%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -6,14 +6,13 @@ public:
 
         string start = strs[0];
         string end = strs[strs.size()-1];
-        string result="";
+        string result = "";
 
-        for(int i = 0 ;i<end.size();i++){
+        for(int i=0;i<end.length();i++){
             if(start[i]!=end[i]){
                 break;
             }
-           result +=start[i];
-
+            result+=start[i];
         }
         return result;
     }
