c8aac8eb5f04d0ac9efc3ceede8b051ede5637c6
RITIK KUMAR <118629689+Ritik-Kumar01@users.noreply.github.com>
2026-02-20 17:16:31 +0530

    Time: 27 ms (11.73%), Space: 14.3 MB (28.37%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -24,43 +24,22 @@ public:
         // }
         // return maxlength;
 
-
-
-        // int left = 0 ;
-        // int maxlength = 0;
-        // unordered_set<int> set;
-
-        // for(int right=0;right<s.length();right++){
-        //    if(set.count(s[right])==0){
-        //     set.insert(s[right]);
-        //     maxlength = max(maxlength,right-left+1);
-        //    }
-        //    else{
-        //     while(set.find(s[right])!=set.end()){
-        //         set.erase(s[left]);
-        //         left++;
-        //     }
-        //    }
-        //    set.insert(s[right]);
-        // }
-        // return maxlength;
-
         int left = 0;
         int maxlength = 0;
-        unordered_set<int>set;
+        unordered_set<int>st;
 
         for(int right=0;right<s.length();right++){
-            if(set.count(s[right])==0){
-                set.insert(s[right]);
-                maxlength = max(maxlength,right-left+1);
+          if(st.count(s[right])==0){
+            st.insert(s[right]);
+            maxlength = max(maxlength,right-left+1);
+          }
+          else{
+            while(st.count(s[right])){
+                st.erase(s[left]);
+                left++;
             }
-            else{
-                while(set.find(s[right])!=set.end()){
-                    set.erase(s[left]);
-                    left++;
-                }
-            }
-            set.insert(s[right]);
+          }
+          st.insert(s[right]);
         }
         return maxlength;
     }
