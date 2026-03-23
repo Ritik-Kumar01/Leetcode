176b56c9f9726c135532a4e5cdd1215e29d456b2
RITIK KUMAR <118629689+Ritik-Kumar01@users.noreply.github.com>
2026-03-23 13:10:14 +0530

    Time: 0 ms (100%), Space: 14.7 MB (52.34%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -16,18 +16,31 @@ public:
     //  return {};
 
 
-    unordered_map<int,int> mp;
+    // unordered_map<int,int> mp;
 
-    for(int i=0;i<nums.size();i++){
-        int complement = target-nums[i];
+    // for(int i=0;i<nums.size();i++){
+    //     int complement = target-nums[i];
+
+    //     if(mp.find(complement)!=mp.end()){
+    //         return {mp[complement],i};
+    //     }
+
+    //     mp[nums[i]]=i;
+    // }
+    // return {};
+       
+
+
+       unordered_map<int,int> mp;
+
+       for(int i = 0 ;i<nums.size();i++){
+        int complement =  target - nums[i];
 
         if(mp.find(complement)!=mp.end()){
             return {mp[complement],i};
         }
-
-        mp[nums[i]]=i;
-    }
-    return {};
-       
+        mp[nums[i]] = i;
+       }
+       return {};
     }
 };
