73510e6919a23d4059a2e23e306f08e4bf8d29b2
RITIK KUMAR <118629689+Ritik-Kumar01@users.noreply.github.com>
2026-03-23 13:40:49 +0530

    Time: 0 ms (100%), Space: 18.1 MB (38.86%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -23,27 +23,49 @@ public:
         // return result;
 
 
-        unordered_map<int,int>freq;
+        // unordered_map<int,int>freq;
 
-        for(int i=0;i<nums.size();i++){
-            freq[nums[i]]++;
+        // for(int i=0;i<nums.size();i++){
+        //     freq[nums[i]]++;
+        // }
+
+        // vector<pair<int,int>>vec;
+
+        // for(auto &it:freq){
+        //     vec.push_back({it.second,it.first});
+        // }
+
+        // sort(vec.begin(),vec.end(),greater<>());
+
+        // vector<int>result;
+
+        // for(int i=0;i<k;i++){
+        //     result.push_back(vec[i].second);
+        // }
+
+        // return result;
+        
+
+
+        unordered_map<int,int> mp;
+
+        for(int i = 0 ;i<nums.size();i++){
+            mp[nums[i]]++;
         }
 
-        vector<pair<int,int>>vec;
+        vector<pair<int,int>> vec;
 
-        for(auto &it:freq){
+        for(auto &it : mp ){
             vec.push_back({it.second,it.first});
         }
 
         sort(vec.begin(),vec.end(),greater<>());
 
-        vector<int>result;
+        vector<int> result;
 
         for(int i=0;i<k;i++){
             result.push_back(vec[i].second);
         }
-
-return result;
-        
+        return result;
     }
 };
